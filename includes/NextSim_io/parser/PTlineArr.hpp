ce83315817581973f9c4bb0e46e61874336f495f
peijiapok <peijiapok@gmail.com>
2025-03-10 19:40:09 +0900

    .

--- includes/NextSim_io/parser/PTlineArr.hpp
+++ includes/NextSim_io/parser/PTlineArr.hpp
@@ -2,44 +2,61 @@
  * NextSim Captain
  * @file : PTlineArr.hpp
  * @version : 1.0
- * @author : ???
+ * @author : 
  */
 
-#ifndef PTROUTEARR_H
-#define PTROUTEARR_H
-
-#include <vector>
-#include <NextSim_io/inputclass/InputPTline.hpp>
-
-namespace NextSimIO
-{
-/**
- * @class PTlineArr
- * @brief Class for whole public transit line information from ptline.xml
-*/
-class PTlineArr
-{
-public:
-    /**
-     * @details Constructor (Parse public transit line information from ptline.xml)
-    */
-    PTlineArr();
-    
-    /**
-     * @details Get vector of public transit lines
-     * @return Vector of public transit lines
-    */
-    std::vector<InputPTline> GetPTLines()
-    {
-        return m_ptLines;
-    }
-
-private:
-    /**
-     * @details Vector of public transit lines
-    */
-    std::vector<InputPTline> m_ptLines;
-};
-} // namespace NextSimIO
-
-#endif
+ #ifndef PTROUTEARR_H
+ #define PTROUTEARR_H
+ 
+ #include <vector>
+ #include <string>
+ #include <NextSim_io/inputclass/InputPTline.hpp>
+ #include <NextSim_io/util/XMLParser.hpp> // Include XML parser utility
+ 
+ namespace NextSimIO
+ {
+ /**
+  * @class PTlineArr
+  * @brief Class for whole public transit line information from ptline.xml
+ */
+ class PTlineArr
+ {
+ public:
+     /**
+      * @details Constructor (Parses public transit line information from ptline.xml)
+      * @param filename Path to the ptline.xml file
+     */
+     explicit PTlineArr(const std::string& filename)
+     {
+         ParseXML(filename);
+     }
+ 
+     /**
+      * @details Get vector of public transit lines
+      * @return Vector of public transit lines
+     */
+     std::vector<InputPTline> GetPTLines() const
+     {
+         return m_ptLines;
+     }
+ 
+ private:
+     /**
+      * @details Vector of public transit lines
+     */
+     std::vector<InputPTline> m_ptLines;
+ 
+     /**
+      * @details Parses public transit line information from an XML file
+      * @param filename Path to the ptline.xml file
+     */
+     void ParseXML(const std::string& filename)
+     {
+         // Use XML parser utility to load transit lines
+         m_ptLines = XMLParser::ParsePTLines(filename);
+     }
+ };
+ } // namespace NextSimIO
+ 
+ #endif
+ 
--- includes/NextSim_io/parser/StationArr.hpp
+++ includes/NextSim_io/parser/StationArr.hpp
@@ -2,14 +2,16 @@
  * NextSim Captain
  * @file : StationArr.hpp
  * @version : 1.0
- * @author : ???
+ * @author : 
 */
 
 #ifndef STATIONARR_H
 #define STATIONARR_H
 
 #include <vector>
+#include <string>
 #include <NextSim_io/inputclass/InputStation.hpp>
+#include <NextSim_io/util/XMLParser.hpp> // Include XML parser utility
 
 namespace NextSimIO
 {
@@ -22,21 +24,31 @@ class StationArr
 
 public:
     /**
-     * @details Constructor (Parse station information from publictransit.xml)
+     * @details Constructor (Parses station information from publictransit.xml)
+     * @param filename Path to the publictransit.xml file
     */
-    StationArr();
+    explicit StationArr(const std::string& filename)
+    {
+        ParseXML(filename);
+    }
 
     /**
      * @details Get vector of stations
      * @return Vector of stations
     */
-    std::vector<InputStation> GetStations() { return m_stations; }
+    std::vector<InputStation> GetStations() const
+    {
+        return m_stations;
+    }
 
     /**
      * @details Get vector of DRT stations
      * @return Vector of DRT stations
     */
-    std::vector<InputDRTStation> GetDRTStations() { return m_drtStations; }
+    std::vector<InputDRTStation> GetDRTStations() const
+    {
+        return m_drtStations;
+    }
 
 private:
     /**
@@ -49,6 +61,15 @@ private:
     */
     std::vector<InputDRTStation> m_drtStations;
 
+    /**
+     * @details Parses station information from an XML file
+     * @param filename Path to the publictransit.xml file
+    */
+    void ParseXML(const std::string& filename)
+    {
+        // Use XML parser utility to load station and DRT station data
+        XMLParser::ParseStations(filename, m_stations, m_drtStations);
+    }
 };
 } // namespace NextSimIO
 
