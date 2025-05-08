f77d5fbb3c6be35640be60507b126b6c645859ee
wjstnwp <wjstnwp123@kaist.ac.kr>
2025-05-08 13:29:03 +0900

    .

--- includes/NextSim_io/inputclass/Railstation/Exit.hpp
+++ includes/NextSim_io/inputclass/Railstation/Exit.hpp
@@ -5,77 +5,77 @@
  * @author : YuseockHwang
  */
 
- #pragma once
- #ifndef Exit_H
- #define Exit_H
- 
- #include <string>
- 
- namespace NextSimIO
- {
- /**
-  * @class Exit
-  * @brief Class for Exit information
-  */
- class Exit
- {
- public:
-     /**
-      * @details Constructor
-      * @param id exit ID
-      * @param link 
-      * @param offset 
-      * @param accesstime 
-      */
+#pragma once
+#ifndef Exit_H
+#define Exit_H
+
+#include <string>
+
+namespace NextSimIO
+{
+/**
+ * @class Exit
+ * @brief Class for Exit information
+ */
+class Exit
+{
+public:
+    /**
+     * @details Constructor
+     * @param id exit ID
+     * @param link 
+     * @param offset 
+     * @param accesstime 
+     */
     Exit(int id, int link, int offset, double accesstime);
- 
-     /**
-      * @details 
-      * @return 
-      */
+
+    /**
+     * @details 
+     * @return 
+     */
     int GetId() { return m_id; }
 
-     /**
-      * @details 
-      * @return 
-      */
+    /**
+     * @details 
+     * @return 
+     */
     int GetLink() { return m_link; }
 
-      /**
-      * @details 
-      * @return 
-      */
+    /**
+     * @details 
+     * @return 
+     */
     int GetOffset() { return m_offset; }
 
-     /**
-      * @details 
-      * @return 
-      */
+    /**
+     * @details 
+     * @return 
+     */
     double GetAccesstime() { return m_accesstime; }
 
- 
- private:
-     /**
-      * @details Exit ID
+
+private:
+    /**
+     * @details Exit ID
      */
     int m_id;
 
-     /**
-      * @details 
+    /**
+     * @details 
      */
     int m_link;
 
-     /**
-      * @details 
+    /**
+     * @details 
      */
     int m_offset;
 
-     /**
-      * @details 
+    /**
+     * @details 
      */
     double m_accesstime;
 
- };
- } // namespace NextSimIO
- 
- #endif
+};
+} // namespace NextSimIO
+
+#endif
