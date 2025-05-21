2f6d8268d451c27e8e067683eeb6e05f62cbafd5
wjstnwp <wjstnwp123@kaist.ac.kr>
2025-05-21 13:52:35 +0900

    add agent passenger id attribute

--- sources/NextSim_io/inputclass/InputPax.cpp
+++ sources/NextSim_io/inputclass/InputPax.cpp
@@ -12,11 +12,10 @@ namespace NextSimIO
 InputODPax::InputODPax(int origin, int dest, int flow, std::string dist) 
     : m_origin(origin), m_dest(dest), m_flow(flow), m_dist(dist) {};
 
-    InputAgentPax::InputAgentPax(int originLink, int originOffset, int destLink, int destOffset,
-      double time, std::string type)
-    : m_originLink(originLink), m_originOffset(originOffset), 
+InputAgentPax::InputAgentPax(int id, int originLink, int originOffset, int destLink, int destOffset, double time)
+    : m_id(id), m_originLink(originLink), m_originOffset(originOffset), 
       m_destLink(destLink), m_destOffset(destOffset), 
-      m_time(time), m_type(type) {}
+      m_time(time) {}
 
 void InputAgentPax::AddTrip(int origin, int dest, std::string mode) {
   m_trips.push_back(InputAgentPaxTrip(origin, dest, mode)); }
